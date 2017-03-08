02dfea76be081a55ba66517633b677a335ad4c21
孟宪亮 <meng.xianliang@immomo.com>
2017-03-08 18:07:38 +0800

    test upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function WebpackPxswitchPlugin(options) {
+
+	// user input options
+	this.options = _.extend({
+		fontsize: options.fontsize || '50', // default => 配置 html => 写在html中
+		overwrite: options.overwrite || true
+	}, options);
+}
+
+WebpackPxswitchPlugin.prototype.apply = function (compiler, callback) {
+	// some code here
+	compiler.plugin('done', function () {
+		console.log('Hello World!');
+		console.log(this.options.toString());
+	});
+}
+
+
+module.exports = WebpackPxswitchPlugin;
