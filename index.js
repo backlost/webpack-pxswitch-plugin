fc6c42a69b435eb7f9c3cbc99bb29231205b1618
孟宪亮 <meng.xianliang@immomo.com>
2017-03-09 15:58:24 +0800

    complete all

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const _ = require('lodash')
-
 function WebpackPxswitchPlugin(options) {
+    // user input options
+    this.options = options.fontsize || '50';
+    this.overwrite = options.overwrite || true;
 
-	// user input options
-	this.options = _.extend({
-		fontsize: options.fontsize || '50', // default => 配置 html => 写在html中
-		overwrite: options.overwrite || true
-	}, options);
 }
 
-WebpackPxswitchPlugin.prototype.apply = function (compiler, callback) {
-	// some code here
-	compiler.plugin('emit', compiltation=>{
-		console.log('Hello World!');
-		console.log(compiltation);
-		console.log(this.options.toString());
-	});
+WebpackPxswitchPlugin.prototype.apply = function (compiler) {
+    // some code here
+    compiler.plugin('emit', (compiltation, callback) => {
+        const baseFont = parseInt(this.options.baseSize || 36)
+        Object.keys(compiltation.assets).forEach((key) => {
+            if (key.slice(-3) == 'css') {
+                let tSource = compiltation.assets[key].source()
+                tSource = tSource.replace(/([0-9]*\.[0-9]+|[0-9]+)px/g, function (number) {
+                    return `${(number.slice(0, -2) / baseFont).toFixed(2)}rem`
+                })
+                compiltation.assets[key] = {
+                    source: function () {
+                        return tSource
+                    },
+                    size: function () {
+                        return tSource.length
+                    }
+                }
+            }
+
+        })
+        //Important
+        callback()
+    })
 }
 
 
-module.exports = WebpackPxswitchPlugin;
+module.exports = WebpackPxswitchPlugin
